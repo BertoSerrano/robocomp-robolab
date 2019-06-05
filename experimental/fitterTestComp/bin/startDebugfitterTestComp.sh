09224cd3941a10b58bdc3fa00d08a9a16c14bbf8
Alberto <albertoserranoleon@gmail.com>
2019-06-05 13:45:49 +0200

    Also purging experimental components

--- experimental/fitterTestComp/bin/startDebugfitterTestComp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./fittertestcomp --Ice.Config=../etc/generic_config.debug,../etc/specific_config.debug &
--- experimental/primeSenseComp/bin/startDebugPrimeSense.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./primesenseComp --Ice.Config=../etc/config.debug & 
